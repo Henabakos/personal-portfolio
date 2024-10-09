651f9daca98a869448788323dea95971f5fb2e86
Henabakos <henogato9876@gmail.com>
2024-10-09 15:20:49 +0300

    added title and image

--- client/src/components/Navabar.jsx
+++ client/src/components/Navabar.jsx
@@ -6,8 +6,11 @@ const Navabar = () => {
   return (
     <nav className="block lg:hidden border-b shadow-md fixed top-0  px-8 w-full bg-gray-50 z-50 ">
       <div className="flex justify-between items-center h-12 ">
-        <FaHome size={24} className="text-blue-500" />
-        <CiLight size={24} className="text-blue-500" />
+        <Link to="/">
+          <FaHome size={24} className="text-blue-500" />
+        </Link>
+
+        <DarkModeToggle />
       </div>
     </nav>
   );
